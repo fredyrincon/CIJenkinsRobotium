6d0133752add1103ff082138c6cdae25a80e6d2f
frincon <rincon>
2015-02-03 16:51:46 +1100

    Add thes to unit testing for roboelectric

--- app/src/androidTest/java/healthconnex/com/au/cijenkinsrobotium/ApplicationRobolectricTest.java
+++ app/src/androidTest/java/healthconnex/com/au/cijenkinsrobotium/ApplicationRobolectricTest.java
@@ -14,6 +14,8 @@ import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.util.ActivityController;
 
+import java_cup.Main;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
@@ -29,7 +31,7 @@ public class ApplicationRobolectricTest  {
 
     private MainActivity activity;
 
-    //private final ActivityController<MainActivity> controller = buildActivity(MainActivity.class);
+    private final ActivityController<MainActivity> controller = buildActivity(MainActivity.class);
 
     @Before
     public void setUp() {
@@ -40,15 +42,59 @@ public class ApplicationRobolectricTest  {
         Assert.assertTrue(Boolean.FALSE);
     }
 
-    /*@Before
-    public void setUp() throws Exception{
-        activity = buildActivity(MainActivity.class).create().get();
+
+   /* //Task of click on a button to open another activity and get back
+    public void testRoboelectricPushNextActivity() throws  Exception {
+
+        /*Activity activity = Robolectric.buildActivity(MainActivity.class).create().get();
+        Button pressNextButton = (Button) activity.findViewById(R.id.buttonNext);
+        pressNextButton.performClick();
+
+        Intent expectedIntent = new Intent(activity, DetailActivity.class);
+        assertThat(shadowOf(activity).getNextStartedActivity()).isEqualTo(expectedIntent);
+
+        solo.goBack();*/
+
+
+       /* MainActivity activity = controller.create().start().resume().get();
+        activity.findViewById(R.id.buttonNext).performClick();
+
+        Intent expectedIntent = new Intent(activity, MainActivity.class);
+        assertThat(shadowOf(activity).getNextStartedActivity()).isEqualTo(expectedIntent);
+    }
+
+    //Task of click on a button to open another activity and get back
+    public void testRoboelectricPushSecondNextActivity() throws  Exception {
+
+        // check that we have the right activity
+        solo.assertCurrentActivity("wrong activity", MainActivity.class);
+
+        // Click a button which will start a new Activity
+        // Here we use the ID of the string to find the right button
+        solo.clickOnButton(solo.getString(R.string.button2));
+
+        // Validate that the Activity is the correct one
+        solo.assertCurrentActivity("wrong activity", SecondDetailActivity.class);
+
+        solo.goBack();
     }*/
 
-   /* @Test
-    public void testTrueIsTrue() throws Exception {
-        assertEquals(true,true);
-    }*/
+    //Task to change the label of the textView
+    public void testRoboelectricChangeLabelData() throws Exception {
+        Activity activity = Robolectric.buildActivity(MainActivity.class).create().get();
+
+        Button pressChangeLabel = (Button) activity.findViewById(R.id.buttonChangeText);
+        TextView results = (TextView) activity.findViewById(R.id.textView);
+
+        pressChangeLabel.performClick();
+        String resultsText = results.getText().toString();
+        assertThat(resultsText, equalTo("Testing Android Rocks!"));
+    }
+
+    //Testing lifecycle
+    public void testRoboelectricLifeCycle() throws  Exception {
+        Activity activity = Robolectric.buildActivity(MainActivity.class).create().start().resume().visible().get();
+    }
 
     /*@Test
     public void testClickingButton_shouldChangeResultsViewText() throws Exception {
